1c513b04b4c550a656f11dc75c5d51514ca2527e
Yasikhorram <khorramyasaman@gmail.com>
2021-05-24 22:00:29 -0700

    parent dashboard

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CampInfo from '../CampInfo/CampInfo.jsx';
+import ParentDashboard from '../ParentDashboard/ParentDashboard.jsx';
 // import all pages that will need to be shown in Dashboard
 
 const Dashboard = () => {
   return (
     <>
     {/* Where we will conditionally render the pages based on link clicked from SideBar */}
-      <CampInfo />
+      {/* <CampInfo /> */}
+      <ParentDashboard />
     </>
   );
 }
