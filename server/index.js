b59a8e4ddfe073f0926938e03d92b486b5dd9344
Will Trinh <williamltrinh@gmail.com>
2021-05-24 21:19:44 -0700

    Implemented InfoBar

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 // const socketio = require('socket.io');
 const http = require('http');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,15 +16,27 @@ const router = require('./routes.js');
 const PORT = process.env.PORT || 3001;
 
 io.on('connection', (socket) => {
-  console.log('new connection!');
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room })
     // error handling
-    const error = true;
-    if (error) {
-      callback({ error: 'error' });
-    }
+    if (error) return callback(error);
+    // tell the user that they have joined the chat
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the ${user.room}` })
+    // tell all other users in the room that the user has joined
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
+
+    socket.join(user.room);
+
+    callback();
   })
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  })
+
   socket.on('disconnect', () => {
     console.log('User had left.');
   })
