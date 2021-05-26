ba1028762dbd8d1e05b73c4b706485c169538b71
Jacky Wu <Jackyw1@uci.edu>
2021-05-25 22:24:50 -0700

    implement hash and encryption

---
+++ server/security/hash.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto');
+const initialize = require('./initialize.js');
+
+// takes in username and password strings
+// hashes password property with type buffer
+const hashPassword = (user) => {
+  user['password'] = crypto.createHash('sha256').update(`${user.password}${user.salt}`).digest();
+};
+
+module.exports = hashPassword;
